83efe34948098a7f983f2d1f3cb54549e3a97c54
Dakshi <dakshiegoel@gmail.com>
2023-07-22 23:46:32 +0530

    updated files

--- src/appwrite/Appwrite.jsx
+++ src/appwrite/Appwrite.jsx
@@ -3,8 +3,8 @@ import { Client, Account, ID, Functions, Databases } from "appwrite";
 const client = new Client();
 
 client
-  .setEndpoint("https://cloud.appwrite.io/v1")
-  .setProject("64a2397f42a2b73da9e4");
+  .setEndpoint(process.env.REACT_APP_ENDPOINT)
+  .setProject(process.env.REACT_APP_PROJECT_ID);
 
 export const account = new Account(client);
 export const functions = new Functions(client);
